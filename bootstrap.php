dc80243d058d8470fbcfa46c2f562e3bcfdb20bd
PDAConcepcion <202311772@fit.edu.ph>
2025-07-15 12:22:07 +0800

    Add path definitions for handlers, utils, and templates in bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,7 @@
 <?php
 define('BASE_PATH', realpath(__DIR__));
+define('HANDLERS_PATH', BASE_PATH . '/handlers');
+define('UTILS_PATH', BASE_PATH . '/utils');
+define("TEMPLATES_PATH", BASE_PATH . '/components/templates');
 
 chdir(BASE_PATH);
