a6014550eee9c49abeacb4f162f6a2e016c0bbd7
gabbycam98 <gabcamino1298@gmail.com>
2025-07-15 18:42:31 +0800

    Add login page template

---
+++ pages/loginPage/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log In</title>
+</head>
+
+<body>
+    <main>
+        <div class="form-container">
+            <div class="logo">
+                <h2>LOGO HERE</h2>
+            </div>
+            <div class="login-field">
+                <form action="" method="post">
+                    <label for="username">Username</label>
+                    <input type="text" name="username" id="username">
+                    <label for="username">Password</label>
+                    <input type="text" name="password" id="password">
+
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+
+            <div>
+
+            </div>
+
+        </div>
+    </main>
+
+</body>
+
+</html>
