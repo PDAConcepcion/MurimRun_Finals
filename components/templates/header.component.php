052cb33835688bb997acf7b1456a0cf0a5f152cb
gabbycam98 <gabcamino1298@gmail.com>
2025-07-15 15:39:33 +0800

    feat: Add header component and styles to index page

---
+++ components/templates/header.component.php
@@ -0,0 +1,10 @@
+<header class="user">
+    <div>
+        <h1>MurimRun</h1>
+    </div>
+    <div></div>
+    <div>
+        <button><a href="">Login</a></button>
+    </div>
+
+</header>
--- index.php
+++ index.php
@@ -3,3 +3,38 @@
 require_once BASE_PATH . '/bootstrap.php';
 require_once HANDLERS_PATH . '/postgreChecker.handler.php';
 require_once HANDLERS_PATH . '/mongodbChecker.handler.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testing</title>
+
+    <link rel="stylesheet" href="/assets/css/style.css">
+
+    <link rel="stylesheet" href="/assets/css/header.css">
+
+
+</head>
+
+<body>
+
+    <main>
+
+        <!-- header -->
+        <?php
+        include TEMPLATES_PATH . '/header.component.php';
+        ?>
+
+        <h1 class="">
+            MurimRum
+        </h1>
+    </main>
+
+</body>
+
+</html>
