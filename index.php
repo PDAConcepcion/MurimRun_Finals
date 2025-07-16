e831db855838d609367a0d2191dff62746dc6fa9
gabbycam98 <gabcamino1298@gmail.com>
2025-07-16 11:55:28 +0800

    refactor: Remove unused content and clean up index page structure

--- index.php
+++ index.php
@@ -30,35 +30,10 @@ require_once HANDLERS_PATH . '/mongodbChecker.handler.php';
         include TEMPLATES_PATH . '/header.component.php';
         ?>
 
-        <div class="content">
+        <div class="">
             <h1 class="">
                 MurimRum
             </h1>
-
-            <div>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae
-                    pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed
-                    diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl
-                    malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad
-                    litora torquent per conubia nostra inceptos himenaeos.
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae
-                    pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed
-                    diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl
-                    malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad
-                    litora torquent per conubia nostra inceptos himenaeos.
-                </p>
-            </div>
-
-        </div>
-
-        <!-- footer -->
-        <?php
-        include TEMPLATES_PATH . '/footer.component.php';
-        ?>
-    </main>
 
 </body>
 
