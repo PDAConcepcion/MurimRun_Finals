e67c1ec9d4999f4618beba80cf1dbda68917723d
gabbycam98 <gabcamino1298@gmail.com>
2025-07-16 14:29:56 +0800

    Refactor layout and styling: adjust content width and encapsulate main content within a div

--- index.php
+++ index.php
@@ -11,9 +11,32 @@ include TEMPLATES_PATH . '/head.component.php';
 
     <?php include TEMPLATES_PATH . '/header.component.php'; ?>
 
-    <h1 class="">
-        MurimRun
-    </h1>
+    <div class="content">
+        <h1 class="">
+            MurimRun
+        </h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem
+            placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar
+            vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc
+            posuere.
+            Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos
+            himenaeos.
+        </p>
+        <p>
+
+            Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem
+            placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar
+            vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc
+            posuere.
+            Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos
+            himenaeos.
+        </p>
+
+        <div class="btn-signup"></div>
+
+    </div>
+
 
 </main>
 
