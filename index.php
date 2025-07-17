fc46da65257b82380f4fa6aeac1fd30d5f626654
PDAConcepcion <202311772@fit.edu.ph>
2025-07-17 15:27:09 +0800

    Improves handler results assignment and CSS paths
    Assigns the results of service checks to variables for later use,
    and standardizes CSS file paths by removing leading slashes.
    Enhances clarity and consistency in asset referencing.

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php
-require_once BASE_PATH . '/bootstrap.php';
-require_once HANDLERS_PATH . '/postgreChecker.handler.php';
-require_once HANDLERS_PATH . '/mongodbChecker.handler.php';
-
 require_once LAYOUTS_PATH . '/main.layout.php';
 
+$mongoCheckerResult = require_once HANDLERS_PATH . '/mongodbChecker.handler.php';
+$postgresCheckerResult = require_once HANDLERS_PATH . '/postgreChecker.handler.php';
+
 $pageCss = [
-    '/assets/css/style.css',
-    '/assets/css/header.css',
-    '/assets/css/footer.css'
+    'assets/css/style.css',
+    'assets/css/header.css',
+    'assets/css/footer.css'
 ];
 
 $services = [
