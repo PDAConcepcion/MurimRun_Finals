2795c0df4e7df82a78e0bda904b04ba6edf6706d
PDAConcepcion <202311772@fit.edu.ph>
2025-07-17 14:46:20 +0800

    Refactors homepage to use layout and inline service data
    Improves maintainability by removing external static service data file and integrating service info directly in the homepage.
    Applies a reusable layout for consistent styling and easier future updates.
    Enhances CSS organization for clearer structure.

--- index.php
+++ index.php
@@ -3,77 +3,68 @@ require_once BASE_PATH . '/bootstrap.php';
 require_once HANDLERS_PATH . '/postgreChecker.handler.php';
 require_once HANDLERS_PATH . '/mongodbChecker.handler.php';
 
+require_once LAYOUTS_PATH . '/main.layout.php';
 
+$pageCss = [
+    '/assets/css/style.css',
+    '/assets/css/header.css',
+    '/assets/css/footer.css'
+];
 
-require_once STATICDATAS_PATH . '/services.staticData.php';
+$services = [
+    [
+        "service" => "Courier Services",
+        "description" => "Slice Through Waiting. Choose MurimRun."
+    ],
+    [
+        "service" => "Realtime Tracking",
+        "description" => "Track your package realtime"
+    ],
+    [
+        "service" => "24/7 Customer Support",
+        "description" => "Talk to one of our agents"
+    ]
+];
 
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MurimRun - Swift as the Blade!</title>
-
-    <link rel="stylesheet" href="/assets/css/style.css">
-
-</head>
-
-<body>
-
-    <main>
-
-
-
-        <div class="content">
-            <div class="intro-background">
-                <img src="assets/img/martial-arts-2400.jpg" alt="MurimRun Background" />
-                <div class="overlay"></div>
-                <div class="intro-container">
-                    <h1 class="">MurimRun</h1>
-
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque
-                        faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi
-                        pretium tellus duis convallis. Tempus leo eu aenean sed diam urna
-                        tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas.
-                        Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut
-                        hendrerit semper vel class aptent taciti sociosqu. Ad litora
-                        torquent per conubia nostra inceptos himenaeos.
-                    </p>
-                    <div class="btn-actions">
-                        <a href="/pages/signupPage/index.php">
-                            <button class="btn">Create Account</button>
-                        </a>
-                        <a href="/pages/loginPage/index.php">
-                            <button class="btn">Log In</button>
-                        </a>
-                    </div>
+renderMainLayout(function () use ($services) { ?>
+    <div class="content">
+        <div class="intro-background">
+            <img src="/assets/img/martial-arts-2400.jpg" alt="MurimRun Background" />
+            <div class="overlay"></div>
+            <div class="intro-container">
+                <h1 class="">MurimRun</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque
+                    faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi
+                    pretium tellus duis convallis. Tempus leo eu aenean sed diam urna
+                    tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas.
+                    Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut
+                    hendrerit semper vel class aptent taciti sociosqu. Ad litora
+                    torquent per conubia nostra inceptos himenaeos.
+                </p>
+                <div class="btn-actions">
+                    <a href="/pages/signupPage/index.php">
+                        <button class="btn">Create Account</button>
+                    </a>
+                    <a href="/pages/loginPage/index.php">
+                        <button class="btn">Log In</button>
+                    </a>
                 </div>
             </div>
-
-            <section class="cards">
-                <?php foreach ($services as $info): ?>
-                    <div class="card-container">
-                        <div class="image-container">
-                            <img src="" alt="">
-                        </div>
-                        <div class="description-container">
-                            <h1><?php echo $info['service'] ?></h1>
-                            <p><?php echo $info['description'] ?></p>
-                        </div>
-                    </div>
-                <?php endforeach ?>
-            </section>
         </div>
 
-    </main>
-
-
-
-</body>
-
-
-</html>
+        <section class="cards">
+            <?php foreach ($services as $info): ?>
+                <div class="card-container">
+                    <div class="image-container">
+                        <img src="" alt="">
+                    </div>
+                    <div class="description-container">
+                        <h1><?php echo $info['service'] ?></h1>
+                        <p><?php echo $info['description'] ?></p>
+                    </div>
+                </div>
+            <?php endforeach ?>
+        </section>
+    </div>
+<?php }, 'MurimRun - Swift as the Blade!', ['css' => $pageCss]);
