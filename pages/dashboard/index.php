e7a638b4f59df0d43a410ccab6f121ea928dc73b
gabbycam98 <gabcamino1298@gmail.com>
2025-07-17 19:55:26 +0800

    Enhance courier section layout and details in dashboard

--- pages/dashboard/index.php
+++ pages/dashboard/index.php
@@ -1,10 +1,18 @@
+<?php
+require_once LAYOUTS_PATH . '/main.layout.php';
+
+$courierInfo = require_once DUMMIES_PATH . '/deliveries.staticData.php';
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Admin dashboard</title>
 
     <link rel="stylesheet" href="/assets/css/style.css">
     <link rel="stylesheet" href="assets/css/dashboard.css">
@@ -15,24 +23,37 @@
     <main>
 
         <section class="courier-section">
+            <?php foreach ($courierInfo as $courier): ?>
+                <div class="courier-container">
 
-            <div class="courier-container">
+                    <div class="courier-photo">
+                        <img src="" alt="profile photo">
+                    </div>
 
-                <div class="courier-photo">
-                    <img src="" alt="profile photo">
+                    <div class="courier-details">
+                        <div>
+                            <h3>Status: </h3>
+                            <p>Origin: </p>
+                            <p>Destination: </p>
+                            <p>Package description: </p>
+                            <p>Weight: </p>
+
+                        </div>
+                        <div>
+                            <h3><?php echo $courier['status']; ?></h3>
+                            <p><?php echo $courier['origin']; ?></p>
+                            <p><?php echo $courier['destination']; ?></p>
+                            <p><?php echo $courier['packagedescription']; ?></p>
+                            <p><?php echo $courier['weight_kg']; ?> kg</p>
+
+
+                        </div>
+                    </div>
+
                 </div>
-
-                <div class="courier-details">
-                    <h3>Delivery ID</h3>
-                    <p>Courier: </p>
-                    <p>Origin: </p>
-                    <p>Destination: </p>
-                    <p>Status:</p>
-
-
-                </div>
-
-            </div>
+                <?php
+            endforeach;
+            ?>
         </section>
 
     </main>
