0963c6ca354edffed9fe6b1eb239c95a4d64971a
gabbycam98 <gabcamino1298@gmail.com>
2025-07-19 02:00:58 +0800

    Add admin dashboard layout and styles for courier section

---
+++ pages/adminDashboardPage/index.php
@@ -0,0 +1,37 @@
+<link rel="stylesheet" href="assets/adminDashboard.css">
+<link rel="stylesheet" href="/assets/css/style.css">
+
+<div class="admin-dash">
+
+
+    <div class="courier-containter">
+
+
+        <!-- Courier tab -->
+        <div class="courier-tab">
+
+            <div class="courier-total">
+                <h2>Number </h2>
+            </div>
+            <div class="courier-available">
+                <h2>available</h2>
+            </div>
+            <div class="courier-ongoing">
+                <h2>Ongoing</h2>
+            </div>
+        </div>
+
+        <div class="deliveries-users">
+            <div class="delivery-stat">
+                <h2>Total Deliveries</h2>
+            </div>
+
+            <div class="user-stat">
+                <h2>Users</h2>
+            </div>
+        </div>
+
+    </div>
+
+
+</div>
