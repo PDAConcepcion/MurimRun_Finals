e417978e612810aaf5cb71828ab8abb7d516762f
gabbycam98 <gabcamino1298@gmail.com>
2025-07-20 20:06:49 +0800

    Refactor login page layout and styles; add background video for enhanced visual appeal

--- pages/loginPage/index.php
+++ pages/loginPage/index.php
@@ -2,15 +2,15 @@
 require_once LAYOUTS_PATH . '/main.layout.php';
 
 $pageCss = [
-        '/assets/css/header.css',
-        '/assets/css/footer.css',
-        'assets/css/login.css',
-        '/assets/css/style.css'
-    ];
+    '/assets/css/header.css',
+    '/assets/css/footer.css',
+    'assets/css/login.css',
+    '/assets/css/style.css'
+];
 
 Auth::init();
 
-if(Auth::check()) {
+if (Auth::check()) {
     header('Location: /index.php');
     echo '<script>alert("User is already logged in, redirecting to index.php");</script>';
     exit;
@@ -18,28 +18,46 @@ if(Auth::check()) {
 
 $error = $_GET['error'] ?? '';
 renderMainLayout(function () use ($error) { ?>
-<div class="form-container">
-        <div class="logo">
-            <h2>LOGO HERE</h2>
+
+    <div class="page">
+        <div class="bg-container">
+
+            <video autoplay muted loop playsinline class="bg-vid"
+                src="/pages/loginPage/assets/img/dragon-in-clouds.1920x1080.mp4"></video>
         </div>
-        <div class="login-field">
-            <?php if ($error === 'invalid_credential'): ?>
-                <div class="error-message" style="color: red; margin-bottom: 10px;">
-                    Invalid username or password.
+        <div class="account-container">
+
+            <div class="login-section">
+                <div class="form-container">
+                    <div class="logo">
+                        <h2>LOGO HERE</h2>
+                    </div>
+                    <div class="login-field">
+                        <?php if ($error === 'invalid_credential'): ?>
+                            <div class="error-message" style="color: red; margin-bottom: 10px;">
+                                Invalid username or password.
+                            </div>
+                        <?php endif; ?>
+
+                        <form action="../../handlers/auth.handlers.php?action=login" method="post">
+                            <label for="username">Username or Email</label>
+                            <input type="text" name="username" id="username" placeholder="Enter Username or Email" required>
+
+                            <label for="password">Password</label>
+                            <input type="password" name="password" id="password" placeholder="Enter Password" required>
+
+                            <div class="btn-group">
+                                <a href="" class="btn" type="submit">Submit</a>
+                            </div>
+                            <div class="account">
+                                <p>New to MurimRun? <a href="" class="create">Create a free account</a></p>
+
+
+                            </div>
+                        </form>
+                    </div>
                 </div>
-            <?php endif; ?>
-
-            <form action="../../handlers/auth.handlers.php?action=login" method="post">
-                <label for="username">Username or Email</label>
-                <input type="text" name="username" id="username" placeholder="Enter Username or Email" required>
-
-                <label for="password">Password</label>
-                <input type="password" name="password" id="password" placeholder="Enter Password" required>
-
-                <div>
-                    <button class="btn" type="submit">Submit</button>
-                </div>
-            </form>
+            </div>
         </div>
     </div>
 <?php }, 'Log In', ['css' => $pageCss]);
