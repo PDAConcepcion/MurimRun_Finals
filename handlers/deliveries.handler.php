da44bd9d11f1b534783a7e0458315f89b56f5cb3
PDAConcepcion <202311772@fit.edu.ph>
2025-07-21 18:25:43 +0800

    Adds dynamic courier selection and delivery form to dashboard
    Improves user dashboard by loading couriers and deliveries from the database,
    enabling users to select/deselect couriers, and providing an interactive form to
    add deliveries. Enhances UI with new styles for courier images, disabled states,
    and action buttons. Supports future scalability and better user interaction.

--- handlers/deliveries.handler.php
+++ handlers/deliveries.handler.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 require_once BASE_PATH . '/bootstrap.php';
 require_once BASE_PATH . '/vendor/autoload.php';
 require_once UTILS_PATH . '/deliveries.util.php';
+require_once UTILS_PATH . '/auth.utils.php';
 require_once UTILS_PATH . '/envSetter.util.php';
 
 Auth::init();
