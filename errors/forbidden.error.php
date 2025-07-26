a93ed18394a86543eb85d2e71942676790e11e64
gabbycam98 <gabcamino1298@gmail.com>
2025-07-26 13:30:43 +0800

    Refactor error pages: streamline HTML structure and enhance CSS for improved layout and styling

--- errors/forbidden.error.php
+++ errors/forbidden.error.php
@@ -1,23 +1,35 @@
-<link rel="stylesheet" href="/assets/css/errors.css">
-<link rel="stylesheet" href="/assets/css/style.css">
+<?php
+http_response_code(403);
 
-<body>
+require_once LAYOUTS_PATH . '/main.layout.php';
+
+
+$pageCss = [
+    '/assets/css/style.css',
+    '/assets/css/errors.css',
+    '/assets/css/header.css',
+    '/assets/css/footer.css'
+];
+
+renderMainLayout(function () {
+    ?>
 
     <div class="background">
-
-        <section class="error-section">
-            <div class="error-message">
-                <h1>403</h1>
-                <h2>Forbidden</h2>
-                <p>You don't have permission to access / on this server.</p>
-                <a href="/index.php">
-                    <button class="btn">
-                        Go Back
-                    </button>
-                </a>
-
-            </div>
-        </section>
-
     </div>
-</body>
+
+    <section class="error-section">
+        <div class="error-message">
+            <h1>403</h1>
+            <h2>Forbidden</h2>
+            <p>You don't have permission to access / on this server.</p>
+            <div class="action">
+
+                <a href="/index.php" class="btn-2 back">Go Back</a>
+            </div>
+
+        </div>
+    </section>
+
+
+
+<?php }, 'Forbidden', ['css' => $pageCss]) ?>
